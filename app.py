9de48d1db9be9e77fc1855c5c1cfdbd9f9450abf
Omkie-111 <omkiepachauli@gmail.com>
2024-05-19 15:13:15 +0530

    Added docstring to the functions

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import streamlit as st
 from main.logic import input_pdf_text, get_gemini_repsonse
 
-# Prompt Template
+def main():
+    st.title("Medical Report Summary Generator")
+    st.text("Upload a medical report PDF to generate a summary.")
 
-input_prompt = """Study the medical report provided and give me a summary where the summary should consist of what is abnormal in this report means what is below or above healthy range
-and because of that what are the effects to the health of the person and for each give some dietary insights.
-"""
+    uploaded_file = st.file_uploader(
+        "Upload Your Resume", type="pdf", help="Please upload the PDF file"
+    )
 
-## streamlit app
-st.title("")
-st.text("Testing")
-uploaded_file = st.file_uploader(
-    "Upload Your Resume", type="pdf", help="Please uplaod the pdf"
-)
-
-submit = st.button("Submit")
-
-if submit:
     if uploaded_file is not None:
-        text = input_pdf_text(uploaded_file)
-        response = get_gemini_repsonse(input_prompt)
-        st.subheader(response)
+        submit = st.button("Submit")
+        if submit:
+            input_prompt = """Study the medical report provided and give me a summary where the summary should consist of what is abnormal in this report means what is below or above healthy range
+            and because of that what are the effects to the health of the person and for each give some dietary insights.
+            """
+            text = input_pdf_text(uploaded_file)
+            response = get_gemini_repsonse(input_prompt)
+            st.subheader(response)
+
+if __name__ == "__main__":
+    main()
