d4e874b04c2a4dfa7225b5955e466f8fc6479448
Omkie-111 <omkiepachauli@gmail.com>
2024-05-19 15:09:12 +0530

    Added docstring to the functions

--- main/logic.py
+++ main/logic.py
@@ -8,12 +8,30 @@ genai.configure(api_key=API_KEY)
 
 
 def get_gemini_repsonse(input_prompt):
+    """
+    Generate a response using the Gemini-Pro model from GenAI based on the input prompt.
+
+    Args:
+    - input_prompt (str): The prompt to generate a response for.
+
+    Returns:
+    - str: The generated response text.
+    """
     model = genai.GenerativeModel("gemini-pro")
     response = model.generate_content(input_prompt)
     return response.text
 
 
 def input_pdf_text(uploaded_file):
+    """
+    Extract text content from a PDF file.
+
+    Args:
+    - uploaded_file (file): The PDF file to extract text from.
+
+    Returns:
+    - str: The extracted text content.
+    """
     reader = pdf.PdfReader(uploaded_file)
     text = ""
     for page in range(len(reader.pages)):
